e337b85a68c09c64d5aa80de7cb4baa2143fd382
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-07-01 19:14:57 -0600

    ✨ Enhance product DTOs and entity: add name and price fields to CreateProductDto and UpdateProductDto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,15 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  
+}
